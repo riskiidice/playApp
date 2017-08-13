b9659c7808266b86b0ceedfc78dc556f5069318b
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-08-13 20:51:49 +0700

    Adding Video Admin model

--- videoApp/admin.py
+++ videoApp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from models import Album,Video
 # Register your models here.
+class AlbumAdmin(admin.ModelAdmin):
+
+    list_display = ['album_title' , 'group']
+    search_fields = ['album_title', 'group']
+    class Meta:
+        model = Album
+
+admin.site.register(Album, AlbumAdmin)
+
+class VideoAdmin(admin.ModelAdmin):
+
+    list_display = ['video_title', 'album']
+    search_fields = ['video_title']
+
+    class Meta:
+        model = Video
+
+admin.site.register(Video, VideoAdmin)
--- videoApp/models.py
+++ videoApp/models.py
@@ -1,13 +1,16 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import Group
 from django.db import models
 
 # Create your models here.
 class Album(models.Model):
-    group = models.CharField(max_length=10)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
     album_title = models.CharField(max_length=255)
     album_description = models.CharField(max_length=255)
     album_logo = models.FileField(upload_to='album/%Y/%m/%d')
+
+    def __unicode__(self):
+        return unicode(self.album_title)
 
     def __str__(self):
         return self.album_title
@@ -20,5 +23,8 @@ class Video(models.Model):
     timestamp = models.DateTimeField(auto_now_add=False, auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True, auto_now=False)
 
+    def __unicode__(self):
+        return unicode(self.video_title)
+
     def __str__(self):
-        return self.video_title+' '+self.album_title
+        return self.video_title+' '+self.album.album_title
